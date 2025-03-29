3ba77841652a9245c7890d8d3629da19188af931
Gautam7-py <gulatig071@gmail.com>
2025-03-30 03:23:35 +0530

    Running Program

--- task_tracker.py
+++ task_tracker.py
@@ -3,11 +3,11 @@ import os
 
 task_list = "data.json"
 
-def add_tasks(task):
-    if task:
+def add_tasks(des):
+    if des:
         new_task ={
             "id" : len(task_list) + 1,
-            "description": task,
+            "description": des,
             "status": "todo"
         }
         task_list.append(new_task)
@@ -92,6 +92,79 @@ def list_tasks_inprogress():
 
 
 
+def main():
+    print("Welcome to Task Tracker")
+    commands = {
+        "list": list_tasks_all,
+        "list-done": list_tasks_done,
+        "list-notdone": list_tasks_notdone,
+        "list-inprogress": list_tasks_inprogress,
+    }
+
+    while True:
+        command = input("task-cli>").strip().split(" ", 1)
+
+        if not command:
+            continue
+
+        elif command in commands:
+            task_list = commands[command]()
+            if task_list:
+                for task in task_list:
+                    print(f"ID: {task['id']}, Description: {task['description']}, Status: {task['status']}")
+            else:
+                print("No tasks found.")
+                
+
+        elif command[0] == "add":
+            if len(command) > 1:
+                add_tasks(command[1])
+            else:
+                print("Usage: add <task_description>")
+
+
+        elif command[0] == "update":
+            if len(command) > 1:
+                command_parts = command[1].split(" ", 1)
+                if len(command_parts) > 1:
+                    try:
+                        task_id = int(command_parts[0])
+                        des = command_parts[1]
+                        update_task(task_id, des)
+                    except ValueError:
+                        print("Invalid task ID. Please provide a number.")
+                else:
+                    print("Usage: update <task_id> <new_description>")
+            else:
+                print("Usage: update <task_id> <new_description>")
+
+
+        elif command[0] == "delete":
+            if len(command) > 1:
+                try:
+                    task_id = int(command[1])
+                    delete_task(task_id)
+                except ValueError:
+                    print("Invalid task ID. Please provide a number.")
+            else:
+                print("Usage: delete <task_id>")
+
+        elif command[0] == "exit":
+            print("Exiting Task Tracker.")
+            break
+        else:
+            print("Unknown command. Available commands: list, add, update, delete, exit")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+        
+                
 
 
         
